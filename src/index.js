aa00bac93c41973c8f14c128e1e4a2bf68ed992d
AngelGM7 <angelgaitan89@gmail.com>
2024-02-27 19:37:15 -0500

    primer backend

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import express from "express"
+import usersRouter from "./routes/users"
+import messagesRouter from "./routes/messages"
 const app= express()
+app.use('/users', usersRouter)
+app.use('/messages', messagesRouter)
 app.listen(3000, () => console.log('server ready on port 3000'))
+
+
+
---
+++ src/routes/users.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+const router = Router () 
+
+router.get('/registro', (req, res) =>{
+})
+
+router.get('/login', (req, res) =>{
+})
+
+router.get('/logout', (req, res) =>{
+})
+
+router.get('/emails', (req, res) =>{
+})
+
+export default router
